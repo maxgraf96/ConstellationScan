24904370fada2e87881818306d0f644492e28e6e
Fabian Fröschl <e1552749@student.tuwien.ac.at>
2016-11-13 15:56:07 +0100

    angles matrix now only shows angles that could possibly appear in the constellation

--- main.m
+++ main.m
@@ -35,27 +35,7 @@ for n = 1:count
    coors(:,n) = [row(1); col(1)];
 end
 
-%calculate angle between all nodes
-angles = zeros(count);
-for a = 1:count
-   mainnode = [coors(1,a), coors(2,a)];
-   for b = 1:count
-       if a ~= b
-           extnode1 = [coors(1,b), coors(2,b)];
-           for c = 1:count
-               if b ~= c
-                   extnode2 = [coors(1,c), coors(2,c)];
-                   vec1 = mainnode - extnode1;
-                   vec2 = mainnode - extnode2;
-                   angles(c,b,a) = acosd(dot(vec1, vec2) / (norm(vec1) * norm(vec2)));
-               end
-           end
-       end
-   end
-end
-angles(isnan(angles)) = 0;
-
-%remove angles that doesnt appear in star constellation
+%save all angles in big Dipper (Großer Wagen)
 angles_bigDipper = [0.1, 3.6;
                     4.8, 7.3;
                     7.9, 10.0;
@@ -95,8 +75,41 @@ angles_bigDipper = [0.1, 3.6;
                     173.7, 174.7;
                     176.0, 177.0;
                     178.6, 180.0];
-               
+anglesCount_bigDipper = 39;
 
+%calculate angle between all nodes
+angles = zeros(count);
+for a = 1:count
+   mainnode = [coors(1,a), coors(2,a)];
+   for b = 1:count
+       if a ~= b
+           extnode1 = [coors(1,b), coors(2,b)];
+           for c = 1:count
+               if b ~= c
+                   extnode2 = [coors(1,c), coors(2,c)];
+                   vec1 = mainnode - extnode1;
+                   vec2 = mainnode - extnode2;
+                   angle = acosd(dot(vec1, vec2) / (norm(vec1) * norm(vec2)));
+                   %only add angles that could possibly appear in the
+                   %constellation
+                   possible = false;
+                   for i = 1 : anglesCount_bigDipper
+                       if angle >= angles_bigDipper(i, 1) && angle <= angles_bigDipper(i, 2)
+                           possible = true;
+                           break
+                       end
+                   end
+                   if possible == true
+                       angles(c,b,a) = angle;
+                   else
+                       angles(c,b,a) = 0;
+                   end
+               end
+           end
+       end
+   end
+end
+angles(isnan(angles)) = 0;        
                     
 %calculate all the possible combinations for this constellation
 starCount = 11;    %How many stars are in the constellation?
