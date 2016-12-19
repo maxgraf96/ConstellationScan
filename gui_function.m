36b7267d53fcf3f0123c3ec9a06f6e7ac9b6210a
Fabian Fröschl <e1552749@student.tuwien.ac.at>
2016-12-20 00:39:13 +0100

    added stars count to gui

--- gui_function.m
+++ gui_function.m
@@ -8,8 +8,12 @@ f = figure('Visible','off',...
 
 btnStart = uicontrol('Style','pushbutton',...
                      'String','Start',...
-                     'Position',[(width/2)-(width/20),height/10,width/10,height/20],...
-                     'Callback',@startbutton_Callback);                
+                     'Position',[(width/2)-(width/40)-(width/10),height/10,width/10,height/20],...
+                     'Callback',@startbutton_Callback);
+btnCount = uicontrol('Style','pushbutton',...
+                     'String','Count',...
+                     'Position',[(width/2)+(width/40),height/10,width/10,height/20],...
+                     'Callback',@countbutton_Callback);
 sldThreshold = uicontrol('Style','slider',...
                          'Position',[width/40,height/5,(width/20)*9,height/20],...
                          'Callback',@thresholdslider_Callback,...
@@ -30,6 +34,10 @@ textAngle = uicontrol('Style','text',...
                       'String','Angle Tolerance: 1.0',...
                       'Position',[(width/40)*21,((height/5) + (height/20)*6)/2,(width/20)*9,height/20],...
                       'FontSize',round((sqrt(width * height)/80)));
+textCount = uicontrol('Style','text',...
+                      'String','0 Stars found',...
+                      'Position',[(width/10)*4,((height/5) + (height/20)*6)/2,(width/10)*2,height/20],...
+                      'FontSize',round((sqrt(width * height)/80)));
 axesThreshold = axes('Units','pixels',...
                      'Position',[width/40, (height/20)*6, (width/20)*9, (height/10)*7],...
                      'XTick',[],...
@@ -43,12 +51,15 @@ axesSolution = axes('Units','pixels',...
 threshold_value = 0;
 angle_tolerance = 1;
 input_label = im2bw(input, threshold_value);
+input_closed = zeros(size(input_label));
 
 %Change units to normalized so components resize automatically.
 f.Units = 'normalized';
 btnStart.Units = 'normalized';
+btnCount.Units = 'normalized';
 sldThreshold.Units = 'normalized';
 sldAngle.Units = 'normalized';
+textCount.Units = 'normalized';
 textThreshold.Units = 'normalized';
 textAngle.Units = 'normalized';
 axesSolution.Units = 'normalized';
@@ -72,11 +83,19 @@ imshow(input_label);
 
 %Callbacks:
     function startbutton_Callback(source, eventdata)
+        input_label = CCL(input_closed);
         solution = main(input_label, input, angle_tolerance);
         
         cla(axesSolution);
         axes(axesSolution);
         imshow(solution);
+    end
+
+    function countbutton_Callback(source, eventdata)
+        input_label = CCL(input_closed);
+        count_closed = max(input_label(:));
+        
+        set(textCount,'String',strcat(num2str(count_closed), ' Stars found'));
     end
 
     function thresholdslider_Callback(source, eventdata)
@@ -87,11 +106,7 @@ imshow(input_label);
         input_bw = im2bw(input, threshold_value);
        
         se = strel('disk',round((sqrt(size(input_bw, 1) * size(input_bw, 2))/100)));
-        input_closed = imdilate(input_bw,se);
-        
-        input_label = CCL(input_closed);
-        count_closed = max(input_label(:));
-       
+        input_closed = imdilate(input_bw,se);       
         
         cla(axesThreshold);
         axes(axesThreshold);
